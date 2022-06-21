36e8e2c21e6125a34b0a48a647ee25d6b68cb764
Dan <daniel.yucua@gmail.com>
2022-06-21 14:52:20 -0400

    fib

--- fib.py
+++ fib.py
@@ -1,3 +1,35 @@
-print("Hello World")
+# Write a function `fib(n)` that takes in a number as an argurment 
+# the function should return the n-th number of the Fibonacci sequence. 
 
-print("poop")
+# non memoized traditional recurrsive fibonacci sequence 
+def fib(n: int) -> int: 
+
+  if (n <= 2): 
+    return 1
+  return fib(n-1) + fib(n-2)
+
+
+# Memoized fibonacci sequence
+# Storage of duplicate sub problems using something like a hashmap 
+# python dictionary
+
+def fibMemo(n:int) -> int: 
+  return fibMemoHelp(n, {})
+
+def fibMemoHelp(n:int, memo:dict) -> int: 
+  # first check for existence inside of the memo
+  # if in memo then return what's in the memo 
+  if (n in memo): 
+    return memo[n]
+
+  # otherwise continue as always
+  if (n <= 2): 
+    return 1
+  
+  # Memoize previous entries 
+  memo[n] = fibMemoHelp(n-1, memo) + fibMemoHelp(n-2, memo)
+  return memo[n]
+
+
+
+print(fibMemo(50))
