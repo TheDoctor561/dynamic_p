592e60639aa1be872311b66473eb78286fb4ba14
Dan <daniel.yucua@gmail.com>
2022-06-30 12:48:23 -0400

    well then

--- tabulation/fib.py
+++ tabulation/fib.py
@@ -1,17 +1,18 @@
 # Tabulized/bottom up version of fibonacci
 
 def fib(n): 
+  if n == 0: 
+    return 0
+  if n == 1: 
+    return 1
 
-  # create an empty python list and fill with zeros
-  table = [0]*(n + 1)
-  table[1] = 1 
+  tab = [0] * (n + 1)
+  tab[1] = 1
 
-  for i in range(n): 
-    table[i+1] += table[i]
-    if (i + 2) <= n: 
-      table[i+2] += table[i]
+  for i in range(2, n + 1): 
+    tab[i] = tab[i-1] +  tab[i-2] 
 
-  return table[n]
+  return tab[n]
 
 print(fib(6)) # 8 
 print(fib(7)) # 13
--- tabulation/gridTraveler.py
+++ tabulation/gridTraveler.py
@@ -8,18 +8,16 @@
 # Write a function `gridTraveler(m,n)` that calculates this. 
 # O(mn) Time and Space 
 
-def gridTraveler(n, m): 
-  table = [[0 for i in range(m + 1)] for j in range(n + 1)]
+def gridTraveler(n, m):   
+  tab = [[1 for i in range(m)] for j in range(n)]
+  
 
-  table[1][1] = 1
+  for i in range(1, n): 
+    for j in range(1, m): 
+      
+      tab[i][j] = tab[i- 1][j] + tab[i][j - 1]
 
-  for i in range(n + 1): 
-    for j in range(m + 1): 
-      curr = table[i][j]
-      if i + 1 <= n: table[i+1][j] += curr
-      if j + 1 <= m: table[i][j+1] += curr 
-  return table[n][m]
-
+  return(tab[n-1][m-1])
 
 
 print(gridTraveler(1,1)) # 1 
